ee4497bfb886f3f75b87f32702c923f2753e65ce
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2025-01-13 17:17:20 +0900

    feat: 일단 커밋

--- Level1/SpringBootDeveloper/src/main/java/org/example/Controller/Member.java
+++ Level1/SpringBootDeveloper/src/main/java/org/example/Controller/Member.java
@@ -14,7 +14,7 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
-    private Long id; // DB 테이블의 'id' 컬럼과 매칭
+    private Long id;
     @Column(name = "name", nullable = false)
-    private String name; // DB 테이블의 'name' 컬럼과 매칭
+    private String name;
 }
